eaceaf1a5ceb729e295806c074d91d3dffa933b1
AplHelp05 <39813679+AplHelp05@users.noreply.github.com>
2020-01-08 12:28:38 -0600

    Bar chart implementation

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,13 +1,14 @@
+import { SalesDataService } from './../../services/salesdata.service';
 import { Component, OnInit } from '@angular/core';
-
-const SAMPLE_BARCHART_DATA: any[] = [
+import * as moment from 'node_modules/moment';
+/*const SAMPLE_BARCHART_DATA: any[] = [
   { data: [65,65,34,23,76,87,56], label: 'Q3 Sales' },
   { data: [65,23,34,23,76,87,55], label: 'Q4 Sales' }
 ];
 
 const SAMPLE_BARCHART_LABELS: string[] = [
   'W1','W2','W3','W4','W5','W6','W7'
-];
+];*/
 
 @Component({
   selector: 'app-bar-chart',
@@ -16,9 +17,14 @@ const SAMPLE_BARCHART_LABELS: string[] = [
 })
 export class BarChartComponent implements OnInit {
 
+  orders: any;
+  ordersLabels: string[];
+  orderData: number[];
+  constructor(private _salesDataService: SalesDataService) { }
 
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+
+  public barChartData: any[];
+  public barChartLabels: string[];
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartOptions: any = {
@@ -26,9 +32,44 @@ export class BarChartComponent implements OnInit {
     responsive: true
   };
 
-  constructor() { }
-
   ngOnInit() {
+    this._salesDataService.getOrders(1, 100).subscribe(
+      res => {
+        const localChartData = this.getChartData(res);
+        console.log(localChartData);
+        this.barChartLabels = localChartData.map(x => x[0]).reverse();
+        this.barChartData = [{ 'data': localChartData.map(x => x[1]), 'label':'Sales' }];
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  getChartData(res: Response) {
+    this.orders = res['page']['data'];
+    const data = this.orders.map(o => o.total);
+    //console.log(data);
+    const labels = this.orders.map(o => moment(new Date(o.placed)).format('YY-MM-DD'));
+
+    //Create a new array with DATE AND TOTAL
+    const formattedOrders = this.orders.reduce((r, e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'),e.total]);
+      return r;
+    }, []);
+
+    const p = [];
+    const chartData = formattedOrders.reduce((r, e) => {
+      const key = e[0]; //the date on the arry formattedOrders
+      if(!p[key]){ 
+        p[key] = e;
+        r.push(p[key]);
+      }else{
+        p[key][1] += e[1];
+      }
+      return r;
+    }, []);
+    return chartData;
   }
 
 }
